8337d15be6acddd75f36f5b2cf5b3a7ed5cc5279
Riyyan Siddiqui <riyyan.s.24@gmail.com>
2024-08-02 02:15:56 +0500

    Finalized

--- script.js
+++ script.js
@@ -22,25 +22,25 @@ subButton.addEventListener("click", (evt) => {
     return;
   }
   Name_input = Name.value;
-  if (weight_input < 1) {
-    alert("Weight cannot be 0 or negative! Please enter correct data!");
-    return;
-  }
   weight_input = weight.value;
-  if (height_input < 1) {
-    alert("Height cannot be 0 or negative! Please enter correct data!");
+  height_input = height.value;
+  if (height_input < 1 || weight_input < 1) {
+    alert(
+      "Height and weight cannot be 0 or negative! Please enter correct data!"
+    );
     return;
   }
-  height_input = height.value;
   calculateBMI();
   Name.value === "";
   weight.value === "";
   height.value === "";
+  outputMesg.innerText = "";
   // body.style.backgroundColor = "red";
 });
 
 const calculateBMI = () => {
   //bmi = weight / Math.pow(height,2);
+  let category = document.createElement("span");
 
   setTimeout(() => {
     loading.innerText = "Calculating .";
@@ -57,29 +57,41 @@ const calculateBMI = () => {
   setTimeout(() => {
     loading.innerText = "Results:";
     let BMI = weight_input / Math.pow(height_input, 2);
-    if (BMI < 18.5)
-      outputMesg.innerText = `${Name_input} your BMI is ${BMI.toFixed(
+    if (BMI < 18.5) {
+      category.innerText = "underweight.";
+      outputMesg.innerText = `${Name_input} your bmi is ${BMI.toFixed(
         1
-      )}, you are UNDERWEIGHT.`;
-    else if (BMI >= 18.5 && BMI <= 24.9)
-      outputMesg.innerText = `${Name_input} your BMI is ${BMI.toFixed(
+      )}, you are `;
+      category.style.color = "yellow";
+    } else if (BMI >= 18.5 && BMI <= 24.9) {
+      category.innerText = "normal weight.";
+      outputMesg.innerText = `${Name_input} your bmi is ${BMI.toFixed(
         1
-      )}, you are NORMAL WEIGHT.`;
-    else if (BMI >= 25 && BMI <= 29.9)
-      outputMesg.innerText = `${Name_input} your BMI is ${BMI.toFixed(
+      )}, you are `;
+      category.style.color = "grey";
+    } else if (BMI >= 25 && BMI <= 29.9) {
+      category.innerText = "overweight.";
+      outputMesg.innerText = `${Name_input} your bmi is ${BMI.toFixed(
         1
-      )}, you are OVERWEIGHT.`;
-    else if (BMI >= 30 && BMI <= 34.9)
-      outputMesg.innerText = `${Name_input} your BMI is ${BMI.toFixed(
+      )}, you are `;
+      category.style.color = "orange";
+    } else if (BMI >= 30 && BMI <= 34.9) {
+      category.innerText = "obese.";
+      outputMesg.innerText = `${Name_input} your bmi is ${BMI.toFixed(
         1
-      )}, you are OBESE.`;
-    else if (BMI >= 35) {
-      outputMesg.innerText = `${Name_input} your BMI is ${BMI.toFixed(
+      )}, you are `;
+      category.color = "#f54747";
+    } else if (BMI >= 35) {
+      category.innerText = "extremely obese.";
+      outputMesg.innerText = `${Name_input} your bmi is ${BMI.toFixed(
         1
-      )}, you are EXTREMLY OBESE.`;
+      )}, you are `;
+      category.style.color = "red";
     } else {
       outputMesg.innerHTML =
         "<i>Error in calculating BMI, Make sure you have entered correct data<i>";
+      outputMesg.style.color = "red";
     }
+    outputMesg.append(category);
   }, 7000);
 };
